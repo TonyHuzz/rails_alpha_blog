f14b6903867accbd6e4a7448a881c22f4263726e
tonyhu <benabcd5779@gmail.com>
2021-09-05 13:32:35 +0800

    model 加入 Schema Information

--- app/models/article.rb
+++ app/models/article.rb
@@ -2,3 +2,16 @@ class Article < ApplicationRecord
   validates :title, presence: true, length: { minimum: 6, maximum: 100 }
   validates :description, presence: true, length: { minimum: 10, maximum: 300 }
 end
+
+
+
+# == Schema Information
+#
+# Table name: articles
+#
+#  id          :bigint           not null, primary key
+#  title       :string
+#  description :text
+#  created_at  :datetime         not null
+#  updated_at  :datetime         not null
+#
