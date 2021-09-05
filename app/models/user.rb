6d922b8c7cc2996e162ad0cb901c31e2d2adceab
tonyhu <benabcd5779@gmail.com>
2021-09-05 13:33:38 +0800

    Add user validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,26 @@
 class User < ApplicationRecord
-
+  validates :username, presence: true,
+            uniqueness: { case_sensitive: false },
+            length: { minimum: 3, maximum: 25 }
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  validates :email, presence: true,
+            uniqueness: { case_sensitive: false },  #case_sensitive: false 表示不管大小寫，只要字與字數皆相同就不能用
+            length: { maximum: 105 },
+            format: { with: VALID_EMAIL_REGEX }
 end
+
+
+
+
+
+
+# == Schema Information
+#
+# Table name: users
+#
+#  id          :bigint           not null, primary key
+#  username    :string
+#  email       :string
+#  created_at  :datetime         not null
+#  updated_at  :datetime         not null
+#
