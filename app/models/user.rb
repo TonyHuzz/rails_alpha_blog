acef07d215a11437661f0e923207fefde17aae45
tonyhu <benabcd5779@gmail.com>
2021-09-05 20:59:27 +0800

    Add secure password to user

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ApplicationRecord
   before_save { self.email = email.downcase }   #self.email 代表每個object的email
+  has_secure_password
   has_many :articles
 
   validates :username, presence: true,
